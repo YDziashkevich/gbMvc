86f1ff3c4bef396a4f5c6669ac6e280b407bc6d9
Y.Dziashkevich <Y.Dziashkevich@gmail.com>
2014-08-09 23:46:19 +0300

    added comments for mainController

--- inc/controllers/main.php
+++ inc/controllers/main.php
@@ -3,35 +3,52 @@
 class MainController extends Controller
 {
     private $captcha;
-    private $form;
     private $messages;
 
     public function __construct()
     {
         parent::__construct();
         $this->captcha = new CaptchaController();
-        $this->form = new GuestbookModel();
         $this->messages = new MessageModel();
     }
 
 
-    public function indexAction()
+    public function indexAction($numPage)
     {
         $formData = array();
+
+        //установка по умолчанию номера страницы сообщений
+        $numPage = ($numPage != null) ? (int)$numPage : 1;
+
+        //проверка отправлена форма, и если да, то ее валидация
         if(!empty($_POST) && isset($_POST["ok"])){
-            $validate = $this->form->validateData();
+            $validate = $this->messages->validateData();
         }
+
+        //если форма валидна, добовляем сообщения
         if(!$validate){
-            $formData["errors"] = $this->form->getErrors();
+            $formData["errors"] = $this->messages->getErrors();
         }else{
             $this->messages->putMessage();
             header('Location: '.$_SERVER['REQUEST_URI']);
             exit();
         }
-        $captcha = CaptchaController::getCaptcha();
-        $formData["captcha"] = $captcha;
-        $formData["messages"] = $this->messages->getMessages();
 
+        //получение среза сообщений для текуще страницы
+        $messages = $this->messages->getMessages();
+        $startIndex = ($numPage-1)*VIEW_DEFAULT_NUM_MESSAGES;
+        $messagesSlice = array_slice($messages, $startIndex, VIEW_DEFAULT_NUM_MESSAGES);
+
+        //генерируем каптчу
+        $captcha = CaptchaController::getCaptcha();
+
+        //формирование данных для вывода формы
+        $formData["sizeStorage"] = count($messages);
+        $formData["captcha"] = $captcha;
+        $formData["messages"] = $messagesSlice;
+        $formData["page"]=$numPage;
+
+        //вывод формы
         $this->view->renderPartial("main", $formData);
     }
 }
